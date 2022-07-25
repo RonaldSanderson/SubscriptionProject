5e85b23c74b6fc8bc7df955803e288363a783a95
Ronald Sanderson <RMSanderson@student.fullsail.edu>
2022-07-24 21:14:52 -0400

    Add project files.

---
+++ SubscriptionProject/Test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="SubscriptionProject.Test" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <h2>Subscription Tracker Application</h2>
+    <br />
+    <p>
+        Subscription Name:
+        <br />
+        <asp:TextBox ID="SubscriptionNameTextbox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="SaveButton" runat="server" Text="Save" OnClick="SaveButton_Click" />
+        <br />
+        <asp:Label ID="SuccessLabel" runat="server" Visible="false">Saved Succesfully!</asp:Label>
+    </p>
+    
+</asp:Content>
---
+++ SubscriptionProject/Test.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SubscriptionProject
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Only when the page firsts loads, hides the user message.
+            if(!IsPostBack)
+            {
+                this.SuccessLabel.Visible = false;
+            }
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            //Tells button to use Sql server
+            AppDomain.CurrentDomain.SetData("SQLServerCompactEditionUnderWebHosting", true);
+            //Names the connection string and tells the button which connection string to use
+            string SqlSubscriptionConnString = ConfigurationManager.ConnectionStrings["SubscriptionConnectionString"].ConnectionString;
+            //Names and makes a new connection to the database
+            SqlConnection newConn = new SqlConnection(SqlSubscriptionConnString);
+            //Opens the new connection
+            newConn.Open();
+            //Creates the command for the insert statement
+            SqlCommand commandSubscription = new SqlCommand("INSERT INTO dbo.Subscription (SubscriptionName) "
+            + "VALUES (@SubscriptionName)", newConn);
+            //Inserting textbox value into the parameter: this is for security purposes to avoid SQL injection
+            commandSubscription.Parameters.AddWithValue("@SubscriptionName", this.SubscriptionNameTextbox.Text);
+            //Creates the command adaptor
+            SqlDataAdapter adapterSubscription = new SqlDataAdapter();
+            //Sets up the insert command
+            adapterSubscription.InsertCommand = commandSubscription;
+            //Runs the insert command
+            commandSubscription.ExecuteScalar();
+            //Closes the connection when everything is done
+            newConn.Close();
+            //Shows a message to a user
+            this.SuccessLabel.Visible = true;
+
+        }
+    }
+}
