9878a1ede013420a15ec34c9e47e324d6d7619d1
Ronald Sanderson <RMSanderson@student.fullsail.edu>
2022-09-15 19:36:08 -0400

    Fixed a bug for the logout button on the add subscription page where I couldn't log out if the fields were empty.

--- SubscriptionProject/Site.Master.designer.cs
+++ SubscriptionProject/Site.Master.designer.cs
@@ -15,13 +15,13 @@ namespace SubscriptionProject
     {
 
         /// <summary>
-        /// Button1 control.
+        /// LogoutButton control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button LogoutButton;
 
         /// <summary>
         /// MainContent control.
